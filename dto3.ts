0b6da97cacaff76cdf30087fc85f5e1cce110c4c
Afungtu carine tanui <afungtuca02@gmail.com>
2026-09-07 23:08:13 -0700

    chore: regenerate prisma client types

---
+++ dto3.ts
@@ -0,0 +1,12 @@
+﻿import { ApiProperty } from '@nestjs/swagger';
+import { IsBoolean, IsNotEmpty, IsString } from 'class-validator';
+
+export class SubmitOnboardingDto {
+  @ApiProperty({
+    example: true,
+    description: 'Confirmation that information is correct',
+  })
+  @IsBoolean()
+  @IsNotEmpty()
+  confirmed: boolean;
+}
